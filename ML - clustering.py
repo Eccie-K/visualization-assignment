e32366575b1b2ba84c37998675f0a418b89f9c02
esther <estherkc100@gmail.com>
2019-11-20 17:12:49 +0300

    created files and deleted a file

--- ML - clustering.py
+++ ML - clustering.py
@@ -1,16 +1,41 @@
+#modcom.co.ke/advertising
 import pandas
-df = pandas.read_csv('african_crises.csv')
+df = pandas.read_csv("african_crises.csv")
 print(df)
+print(df.shape)
 
 empties = df.isnull().sum()
 print(empties)
 
+#filling empties and encoding where necessary
+# no feature selection in clustering
 subset = df[['exch_usd', 'inflation_annual_cpi']]
 
 array = subset.values
-X = array[:, 0:2]
+X = array[:, 0:2] # no response y hence unsupervised
 
 from sklearn.cluster import KMeans
-model = KMeans(n_clusters=3, random_state = 42) #we do four clusters
+model = KMeans(n_clusters=3, random_state =42) #we do four clusters
 model.fit_predict(X)
 print('Learning//Done')
+
+#Get the means for each cluster and per column
+
+centronoids = model.cluster_centers_
+
+dataframe = pandas.DataFrame(centronoids, columns =['exch_usd', 'inflation_annual_cpi'])
+print(dataframe)
+
+# who is this cluster
+#adding a column at the end
+subset['label'] = model.labels_
+subset = subset[subset['label'] == 2]
+print(subset)
+
+#generating excel worksheet
+
+import xlwt
+subset.to_excel('cluster3.xls', columns = ['exch_usd', 'inflation_annual_cpi'])
+
+#powerBI....
+#modcom.co.ke/datascience/final_project2019
